47c7e0dd18128344bfba61e984d05b3786b6db38
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2018-01-22 17:16:17 +1100

    Implemented API calls using service class

--- bff/src/main/java/me/stockwells/yougo/YougoResource.java
+++ bff/src/main/java/me/stockwells/yougo/YougoResource.java
@@ -1,18 +1,32 @@
 package com.github.stocky37.yougo;
 
 import io.dropwizard.testing.FixtureHelpers;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Path("/")
 @Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class YougoResource {
+
+  private static final Logger log = LoggerFactory.getLogger(YougoResource.class);
 
   @GET
   @Path("gos/{user}")
   public String listGos() {
     return FixtureHelpers.fixture("com.github.stocky37/yougo/fixtures/gos.json");
+  }
+
+  @POST
+  @Path("gos")
+  public String addGo(String json) {
+    log.info(json);
+    return json;
   }
 }
