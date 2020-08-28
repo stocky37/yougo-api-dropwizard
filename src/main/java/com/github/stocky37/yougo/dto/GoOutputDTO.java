9b7602e83da6fcbe09dc6f05831f2d7fce9ce4c1
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2020-08-28 12:02:38 +0100

    use spotbugs & checkstyle

--- src/main/java/com/github/stocky37/yougo/dto/GoOutputDTO.java
+++ src/main/java/com/github/stocky37/yougo/dto/GoOutputDTO.java
@@ -1,5 +1,8 @@
 package com.github.stocky37.yougo.dto;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
+@SuppressFBWarnings("UrF")
 public class GoOutputDTO extends GoInputDTO {
 	public String id;
 }
